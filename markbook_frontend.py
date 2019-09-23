4b670467e7fc5959e968a87a546a39c80ab1ab09
Ryan Kim <43964171+Kimchooman@users.noreply.github.com>
2019-09-23 14:09:28 -0400

    Update markbook_frontend.py

--- markbook_frontend.py
+++ markbook_frontend.py
@@ -3,6 +3,14 @@ import json
 import pprint
 import time
 
+def initialize_session(parser_obj_shell):
+    print("""--New Session-- \n \n
+    Please enter your desired class's course code... \n""")
+
+    course_code = input("--> ")
+
+    parser_obj_shell = parser(f"{course_code}.json")
+
 class parser:
     def __init__(self,filename):
 
@@ -29,28 +37,73 @@ class parser:
             json.dump(self.data, f)
 
 class print_center:
-    def __init__(self,jsonfile):
-        
-        with open(jsonfile, "r") as json_file:
-            responces = json.load(json_file)
-    
+    def __init__(self):
+        self.filler = None
+
     def help(self):
         print("""Commands List:
+          > exit
+          > help
           > add-student
           > remove-student
           > add-assignment
           > calculate-student-avg
-          > generate-student-report""")
+          > generate-student-report \n""")
 
     def add_student(self):
+        fields = []
+
+        fields.append(input("Student First Name -->"))
+        fields.append(input("Student Last Name -->"))
 
     def remove_student(self):
+        fields = []
+        
+        fields.append(input("Student First Name -->"))
+        fields.append(input("Student Last Name -->"))
 
-    def add_assignemnt(self):
-    
+    def add_assignment(self):
+        fields = []
+
+        fields.append(input("Name of the assignemnt -->"))
+        fields.append(input("Due date -->"))
+        fields.append(input("Points the assignemnt is out of -->"))
+
     def calculate_student_avg(self):
-    
+        fields = []
+
+        fields.append(input("Student First Name -->"))
+        fields.append(input("Student Last Name -->"))
+
     def generate_student_report(self):
+        fields = []
+
+        fields.append(input("Student First Name -->"))
+        fields.append(input("Student Last Name -->"))
+
+    def update(self, cmd):
+
+        if cmd == "help":
+            self.help()
+        
+        elif cmd == "add-student":
+            self.add_student()
+
+        elif cmd == "remove-student":
+            self.remove_student()
+
+        elif cmd == "add-assignment":
+            self.add_assignment()
+
+        elif cmd == "calculate-student-avg":
+            self.calculate_student_avg()
+
+        elif cmd == "generate-student-report":
+            self.generate_student_report()
+
+        else:
+            print("Unknown command..... \n")
+            self.help()
 
 class markbook_setup:
     def __init__(self, filename):
@@ -61,16 +114,16 @@ class markbook_setup:
         print("Initializing Markbook, fill in requests fields.")
         time.sleep(self.wait_seconds)
 
-        self.teacher = input("Teacher --> ")
+        self.teacher = input("Teacher -->  ")
         time.sleep(self.wait_seconds)
 
-        self.course_code = input("Course Code --> ")
+        self.course_code = input("Course Code -->  ")
         time.sleep(self.wait_seconds)
 
-        self.course_name = input("Course Name --> ")
+        self.course_name = input("Course Name -->  ")
         time.sleep(self.wait_seconds)
 
-        self.period = input("What period is it --> ")
+        self.period = input("What period is it -->  ")
         time.sleep(self.wait_seconds + 2)
 
         temp_dict = {
@@ -83,8 +136,20 @@ class markbook_setup:
         with open(self.filename, "w") as f:
             json.dump(temp_dict, f)       
 
-while True:
+session_data = None
+
+initialize_session(session_data)
+
+pc = print_center()
+print(" \n Type \"help\" and press Enter to see command list. \n")
+
+while True:    
 
     user_input = input()
 
-    if 
+    user_input = user_input.lower()
+
+    if user_input == "exit":
+        break
+
+    pc.update(user_input)
