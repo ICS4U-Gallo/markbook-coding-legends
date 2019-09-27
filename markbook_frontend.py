a1dad7628595de54a14477cad6359fcc4d136db1
Ryan Kim <43964171+Kimchooman@users.noreply.github.com>
2019-09-27 16:02:12 -0400

    Update markbook_frontend.py

--- markbook_frontend.py
+++ markbook_frontend.py
@@ -1,46 +1,54 @@
+from markbook import *
 import json
 
 def initialize_session(parser_obj_shell):
-    print("""--New Session-- \n \n
-    Please enter your desired class's course code... \n""")
 
-    course_code = input("--> ")
+    while True:
 
-    parser_obj_shell = parser(f"{course_code}.json")
+        req = input("Do you want to load previous session \"o\" or \"c\" create a new classroom? -->")
+
+        if  req == "o" or req == "O":
+
+            code = input("What is the courses\' code? -->  ") + ".json"
+
+            markbook = markbook_setup(code)
+
+            parser_obj_shell = parser(code)
+
+            break
+
+        if req =="c" or req == "C":
+
+            code = input("What is the course code of the classroom you would like to load? -->  ") + ".json"
+
+            parser_obj_shell = parser(code)
+
+            break
+
+        else:
+            print("Error - unknown request, enter either \"o\", or \"c\".")
 
 class parser:
     def __init__(self,filename):
 
         self.filename = filename
         
-        with open(filename, 'r') as json_file:
+        with open(self.filename, 'r') as json_file:
             self.data = json.load(json_file)
-
-        self.student_list = self.data["student_list"]
-        self.assignment_list = self.data["assignment_list"]
-        self.course_code = self.data["course_code"]
-        self.course_name = self.data["course_name"]
-        self.period = self.data["period"]
-        self.teacher = self.data["teacher"]
-
-    def update_dict_surface_info(self, key, value):
-        self.data[key] = value
-
-    def update_dict_sub_info(self, key, value, index):
-        self.data[key][index] = value
 
     def save_data(self):
         with open(self.filename,"w") as f:
             json.dump(self.data, f)
 
-class print_center:
-    def __init__(self):
-        self.filler = None
+class input_center:
+    def __init__(self, parser_obj):
+        self.parser = parser_obj
 
     def help(self):
         print("""Commands List:
           > exit
           > help
+          > save
           > add-student
           > remove-student
           > add-assignment
@@ -54,10 +62,18 @@ class print_center:
         fields.append(input("Student Last Name -->"))
 
     def remove_student(self):
-        fields = []
         
-        fields.append(input("Student First Name -->"))
-        fields.append(input("Student Last Name -->"))
+        while True:
+
+            fields = []
+        
+            fields.append(input("Student First Name -->"))
+            fields.append(input("Student Last Name -->"))
+
+            for student in self.parser.student_list:
+
+                if student["first_name"] == fields[0] and student["last_name"] == fields[1]:
+                    remove_student_from_classroom(student, self.parser.data)
 
     def add_assignment(self):
         fields = []
@@ -65,6 +81,10 @@ class print_center:
         fields.append(input("Name of the assignemnt -->"))
         fields.append(input("Due date -->"))
         fields.append(input("Points the assignemnt is out of -->"))
+
+        temp_assignnment = create_assignment(fields[0],fields[1],fields[2])
+
+        self.parser.data["assignment_list"].append(temp_assignnment)
 
     def calculate_student_avg(self):
         fields = []
@@ -98,6 +118,9 @@ class print_center:
         elif cmd == "generate-student-report":
             self.generate_student_report()
 
+        elif cmd == "save":
+            self.parser.save_data()
+
         else:
             print("Unknown command..... \n")
             self.help()
@@ -123,21 +146,17 @@ class markbook_setup:
         self.period = input("What period is it -->  ")
         time.sleep(self.wait_seconds + 2)
 
-        temp_dict = {
-            "teacher": self.teacher,
-            "course_code": self.course_code,
-            "course_name": self.course_name,
-            "period": self.period
-        }
+        classroom = create_classroom(self.course_code, self.course_name, self.period, self.teacher)
 
         with open(self.filename, "w") as f:
-            json.dump(temp_dict, f)       
+            json.dump(classroom, f)       
 
 session_data = None
 
-initialize_session(session_data)
+initialize_session(sesssion_data)
 
-pc = print_center()
+pc = input_center(session_data)
+
 print(" \n Type \"help\" and press Enter to see command list. \n")
 
 while True:    
@@ -147,6 +166,7 @@ while True:
     user_input = user_input.lower()
 
     if user_input == "exit":
+        session_data.save_data()
         break
 
     pc.update(user_input)
