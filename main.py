c139c895a607314b9c178764dcbd770441f77fea
Ryan Kim <43964171+Kimchooman@users.noreply.github.com>
2019-09-30 15:23:33 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,111 +1,22 @@
-"""
-Markbook Application
-Group members: Alex, Aryan, Ryan
-"""
+from data_managment_methods import initialize_session
+from user_interface_center import user_interface_controller
 
-from typing import Dict
+session_data, filename = initialize_session()
 
-def create_student(first_name: str, last_name: str) -> Dict:
-    """Create student information represented in a dictionary
+usr_interface = user_interface_controller(session_data, filename)
 
-    Args:
-        first_name: first name of the student
-        last_name: last name of the student
-    Returns:
-        Student information as a dictionary
-    """
-    student = {
-        "first_name": first_name,
-        "last_name": last_name,
-        "assignment_list": []
-    }
+msg = "\nNow interacting with %s, enter \"help\" for the list of commands\n" % (session_data["course_code"])
 
-    return student
+print(msg)
 
-def create_assignment(name: str, due: str, points: int) -> Dict:
-    """Creates an assignment represented as a dictionary
-    
-    Args:
-        name: the name of the assignment.
-        due: the due date for the assignment.
-        points: what the assignment is out of (denominator).
-    Returns:
-        Assignment as a dictionary.
-    """
-    return {"name": name,
-            "due": due,
-            "points": points}
+while True:
 
-def create_classroom(course_code: str, course_name: str, period: int, teacher: str) -> Dict:
-    """Creates a classroom dictionary"""
-    classroom = {"course_code": course_code,
-                 "course_name": course_name, 
-                 "period": period,
-                 "teacher": teacher, 
-                 "student_list": [], 
-                 "assignment_list": []
-                }
-    return classroom
+	user_input = input("\n")
 
-def calculate_average_mark(student: Dict, classroom: Dict) -> float:
-    """Calculates the average mark of a student"""
-    
-    sum = 0
-    length = len(student["assignment_list"])
+	user_input = user_input.lower()
 
-    if length is not 0:
-            
-        for i in range(length):
+	if user_input == "exit":
+		usr_interface.update("save")
+		break
 
-            numerator = student["assignment_list"][i]["points"]
-            denumerator = classroom["assignment_list"][i]["points"]
-
-            sum += numerator / denumerator
-
-        return sum / length
-
-    else:
-        return -1
-        
-def add_student_to_classroom(student: Dict, classroom: Dict):
-    #Adds student to a classroom
-    classroom["student_list"].append(student)
-    pass
-
-def remove_student_from_classroom(student: Dict, classroom: Dict):
-    """Removes student from classroom
-
-    Args:
-        student: The student to be removed
-        classroom: the class from which the student will be removed.
-    """
-    classroom["student_list"].remove(student)
-    pass
-
-def add_student_mark(assignment_dict: Dict, mark: int):
-	"""Edits student assgiment info
-	
-	Args:
-		-assignment_dict as an dictionary
-		-mark as a integer
-	
-	return:
-		-None
-	
-	"""
-
-	assignment_dict["points"] = mark
-		
-
-def edit_student(classroom: Dict, **kwargs: Dict):
-    """Edits the student's info with the provided key/value pairs
-    
-    Args:
-        student: The student whose data needs to be udated.
-        **kwargs: KeyWordARGumentS. The key/value pairs of the
-            data that needs to be changed. Can come in the form
-            of a dictionary.
-    """
-    for key in kwargs.keys():
-        student[key] = kwargs[key]
-    pass
+	usr_interface.update(user_input)
